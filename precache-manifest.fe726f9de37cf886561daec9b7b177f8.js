b0ac9497bd194b91c7676420785b8b487415ce11
Nishan Dhakal <ndhakal54@outlook.com>
2020-06-25 22:19:08 -0400

    Updates

---
+++ precache-manifest.fe726f9de37cf886561daec9b7b177f8.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bb5d40c3796dd5a0363eb1ff3fd993b7",
+    "url": "/To-Do-App/index.html"
+  },
+  {
+    "revision": "02ed1af08ff35140e2e8",
+    "url": "/To-Do-App/static/css/main.031e540d.chunk.css"
+  },
+  {
+    "revision": "2f58a026614d36d51ce7",
+    "url": "/To-Do-App/static/js/2.fedf2b09.chunk.js"
+  },
+  {
+    "revision": "809f8196affeaac0e5176b72b79c267c",
+    "url": "/To-Do-App/static/js/2.fedf2b09.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "02ed1af08ff35140e2e8",
+    "url": "/To-Do-App/static/js/main.be13acbc.chunk.js"
+  },
+  {
+    "revision": "522186ac776c7d0de7c9",
+    "url": "/To-Do-App/static/js/runtime-main.68038795.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/To-Do-App/static/media/logo.5d5d9eef.svg"
+  }
+]);
